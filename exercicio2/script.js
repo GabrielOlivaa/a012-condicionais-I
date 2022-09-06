3c7a45d3a99ef504bbe077f681aba309998e7d48
GabrielOlivaa <gedfto@gmail.com>
2022-09-06 20:09:31 -0300

    exercicios ok

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,26 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade= prompt()
+let ensinoMedio= confirm ("voce ja terminou ensino medio?")
+let faculdade = confirm ("voce  esta fazendo faculdade?")
 
-if(){
+if(idade>=18){
+    console.log("Você ja é maior de idade")
 
-}else{
+}else 
+{console.log("voce ainda é menor de idade")
 	
+}
+
+if(ensinoMedio === true){
+    console.log ("Você ja terminou o Ensino medio =D")
+}else  {
+    
+        console.log("Você ainda esta cursando o Ensino medio")
+    }
+
+if(faculdade === false){
+    console.log("Você não esta cursando nenhuma faculdade")
+    
+}else {
+    console.log("Você esta cursando faculdade =D")
+
 }
